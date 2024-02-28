bc9b68b164cc181c8fa1b7e24641da7f397821c4
Amaan Ansari <ansari028amaan@gmail.com>
2024-02-28 18:16:59 +0530

    add user status

--- frontend/src/components/Messages/MessageContainer.jsx
+++ frontend/src/components/Messages/MessageContainer.jsx
@@ -1,12 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useConversation from "../../zustand/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
 import { useAuthContext } from "../../context/AuthContext";
+import { useSocketContext } from "../../context/socketContext";
+import { MdVideoCall } from "react-icons/md";
 const MessageContainer = () => {
     const { selectedConversation, setSelectedConversation } = useConversation()
-
+    const { onlineUsers } = useSocketContext()
+    const [status, SetStatus] = useState('')
+    const videoCall = () => {
+        console.log('video call')
+    }
+    useEffect(() => {
+        const isOnline = onlineUsers.some(user => user.toString() === selectedConversation?._id.toString());
+        SetStatus(isOnline ? 'online' : 'offline');
+    }, [onlineUsers, selectedConversation]);
     useEffect(() => {
         // cleanup unmount
         return () => setSelectedConversation(null)
@@ -15,8 +25,10 @@ const MessageContainer = () => {
         <div className='md:min-w-[450px] flex flex-col border border-gray-300'>
             {!selectedConversation ? <NoChatSelected /> : <>
                 {/* Header */}
-                <div className='bg-slate-500 px-4 py-2 mb-2'>
-                    <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
+                <div className='bg-slate-500 px-4 py-2 mb-2 flex justify-between items-center'>
+                    <span className='label-text text-gray-900 font-bold'>{selectedConversation.fullName}</span>
+                    <span className="label-text text-gray-900 font-bold">{status}</span>
+                    <div><MdVideoCall className="cursor-pointer text-2xl hover:text-gray-900" onClick={videoCall} /></div>
                 </div>
 
                 <Messages />
